13f85a739bcdc3910fe23a98fabd17c2d49746ed
Laura <lhernan53112@gmail.com>
2021-01-09 14:20:03 -0500

    returns an increasing seq

---
+++ src/main/java/com/sequence/sequence_search/App.java
@@ -0,0 +1,51 @@
+package com.sequence.sequence_search;
+
+//write a Java code to find the longest increasing subsequence 
+//from a list of random numbers
+
+public class App 
+{
+	
+	
+    public static void main( String[] args )
+    {
+    	int[] numbers1 = {5,7,9,4,7,94,45,6};
+       
+       selectionSort(numbers1);
+      
+       
+    }
+   
+    static void selectionSort(int[] numbers ) {
+
+        // Put the number in incrementing order
+        // Loop through our numbers
+        for (int i = 0; i < numbers.length ; i++) {
+
+            // This is one of those numbers
+            int outer = numbers[i];
+
+            // Lets find if we can go lower?
+            int lowest = outer;
+            int key = i;
+            for (int k = i + 1; k < numbers.length; k++) {
+                int inner = numbers[k];
+                if (inner < lowest) {
+                    lowest = inner;
+                    key = k;
+                }
+            }
+
+            // Swap them out
+            numbers[i] = lowest;
+            numbers[key] = outer;
+
+        }
+
+        for(int i: numbers) {
+            System.out.println("Number: " + i);
+        }
+
+    }
+    
+}
