426195cc6ba691e41d96e272ecccb34fe97292cc
Laura <lhernan53112@gmail.com>
2021-01-10 12:12:20 -0500

    Added a remove duplicates method

--- src/main/java/com/sequence/sequence_search/App.java
+++ src/main/java/com/sequence/sequence_search/App.java
@@ -1,5 +1,7 @@
 package com.sequence.sequence_search;
 
+import java.util.*;
+
 //write a Java code to find the longest increasing subsequence 
 //from a list of random numbers
 
@@ -10,14 +12,13 @@ public class App
     public static void main( String[] args )
     {
     	int[] numbers1 = {5,7,9,4,7,94,45,6};
-       
-       selectionSort(numbers1);
-      
-       
+    
+      selectionSort(numbers1); 
+      removeDuplicates(numbers1);
+        
     }
    
     static void selectionSort(int[] numbers ) {
-
         // Put the number in incrementing order
         // Loop through our numbers
         for (int i = 0; i < numbers.length ; i++) {
@@ -34,8 +35,8 @@ public class App
                     lowest = inner;
                     key = k;
                 }
-            }
 
+            }
             // Swap them out
             numbers[i] = lowest;
             numbers[key] = outer;
@@ -46,6 +47,20 @@ public class App
             System.out.println("Number: " + i);
         }
 
+    }
+    
+    static void removeDuplicates(int[] numbers ) {
+    	ArrayList<Integer> newNums = new ArrayList<Integer>(); // Create an ArrayList object
+        
+    	for (int j = 1; j < numbers.length ; j++) {
+        	if (numbers[j]!= numbers[j-1]) {
+        		newNums.add(numbers[j]);
+        		}
+        	}
+    	//Prints integer list w/o duplicates      		
+    	for (Integer i : newNums) {
+    	      System.out.println(i);
+    	    }
     }
     
 }
